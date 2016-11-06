c3cf932c4f84977031caaa9f9c290c86fa1d58e0
ShiZniTx <sshiznit@yahoo.com>
2016-11-06 10:36:48 +0200

    Update raport1.php

--- raport1.php
+++ raport1.php
@@ -1,9 +1,26 @@
 <?php
-$sql = "SELECT [Id]
-      ,[GroupCode]
-      ,[BaseUnit]
-      ,[Name]
-  FROM [RockCaffe].[dbo].[InventoryItems]";
+$sql = "SELECT [InventoryItemName] as ItemName,
+      CONVERT(INT,ROUND(CASE WHEN [BaseUnit] = [TransactionUnit] THEN
+        (ISNULL([PhysicalInventory],([InStock]+[Added]-[Removed]-[Consumption]))+
+		ISNULL((SELECT TOP 1 [Quantity] FROM [InventoryTransactions]
+	     WHERE [InventoryItem_Id] = [PeriodicConsumptionItems].InventoryItemId AND [Date] >= CONVERT(varchar(10),GETDATE(),121)
+	     ORDER BY [Id] DESC),0))
+      ELSE
+        ISNULL([PhysicalInventory],([InStock]+[Added]-[Removed]-[Consumption]))*[UnitMultiplier]
+	  END,0)) as InStock,
+	  [BaseUnit] as Unit,
+	  CONVERT(numeric(6,2),([Cost]/[UnitMultiplier])) as UnitCost,
+	  CONVERT(numeric(6,2),ROUND([Cost]*ISNULL([PhysicalInventory],([InStock]+[Added]-[Removed]-[Consumption])),2)) as TotalCost
+
+FROM [PeriodicConsumptionItems]
+
+LEFT OUTER JOIN [InventoryItems] ON [InventoryItemId] = [InventoryItems].[Id]
+
+WHERE [PeriodicConsumptionId] = (SELECT TOP 1 [Id] FROM [RockCaffe].[dbo].[PeriodicConsumptions] ORDER BY [Id] DESC)
+      AND
+	ISNULL([PhysicalInventory],([InStock]+[Added]-[Removed]-[Consumption])) >= 0.05
+
+ORDER BY [InventoryItemName]";
 $stmt = sqlsrv_query( $conn, $sql );
 if( $stmt === false) {
     die( print_r( sqlsrv_errors(), true) );
